ec604a75a957c95ee119888f5e4bfd1a7a5250d4
MUKESHKUMAR A <103376155+MUKESHKUMAR-A@users.noreply.github.com>
2023-04-19 15:26:06 +0530

    First Commit

---
+++ app/src/main/java/com/example/mkmusic/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package com.example.mkmusic;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mkmusic.Activity.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_spalshscreen);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreenActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
